9cf9feba6afa7705fa94b52a3ad8f6aff14d735c
Asim Hanif <asimjutt2003@gmail.com>
2026-07-12 16:58:09 +0500

    fix: resolve TextDirection conflict in CustomerProfileScreen

--- lib/screens/profile/customer_profile_screen.dart
+++ lib/screens/profile/customer_profile_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:easy_localization/easy_localization.dart';
+import 'package:easy_localization/easy_localization.dart' hide TextDirection;
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../core/theme/app_colors.dart';
